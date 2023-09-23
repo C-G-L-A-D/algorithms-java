417281a5add22daaca137b9018f3ff278f29e78c
lad <ladqmpw@163.com>
2023-09-23 20:18:46 +0800

    增加测试用例

---
+++ DataStructure/src/MyLinkedList.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+
+public class MyLinkedList {
+
+//    private MyDoubleLinkedList<Integer> linkedList;
+    private MyArrayList<Integer> linkedList;
+    public MyLinkedList() {
+//        linkedList = new MyDoubleLinkedList<>();
+        linkedList = new MyArrayList<>(10);
+    }
+
+    public int get(int index) {
+        return linkedList.get(index);
+    }
+
+    public void addAtHead(int val) {
+        linkedList.addFirst(val);
+    }
+
+    public void addAtTail(int val) {
+        linkedList.addLast(val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        linkedList.add(index, val);
+    }
+
+    public void deleteAtIndex(int index) {
+        linkedList.remove(index);
+    }
+
+    public void printLinkedList() {
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList1 = new MyLinkedList();
+
+        linkedList1.addAtHead(10);
+        linkedList1.addAtTail(20);
+        linkedList1.addAtIndex(0, 15);
+        linkedList1.printLinkedList();
+    }
+}
